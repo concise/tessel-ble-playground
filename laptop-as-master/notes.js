c347e34917dd25cbc01ec754af41d306f83918ce
Jong-Shian Wu <js@jong.sh>
2016-05-05 19:17:06 +0800

    -

--- laptop-as-master/notes.js
+++ laptop-as-master/notes.js
@@ -1,31 +1,32 @@
 global.noble = require('noble');
 
-global.peripherals = [];
+global.devices = [];
 
-noble.on('discover', function (p) {
-    peripherals.push(p);
-    console.log('Discover one peripheral');
+noble.on('discover', function (d) {
+  devices.push(d);
+  console.log('Discover one peripheral');
 });
 
 noble.on('scanStop', function () {
-    console.log('Scanning operation is stopped');
+  console.log('Scanning operation is stopped');
 });
 
 noble.on('scanStart', function () {
-    console.log('Scanning operation is started');
-    setTimeout(function () {
-        noble.stopScanning();
-    }, 3000);
-    console.log('Scanning operation will be stopped in 3 seconds');
+  console.log('Scanning operation is started');
+  setTimeout(function () {
+    noble.stopScanning();
+  }, 3000);
+  console.log('Scanning operation will be stopped in 3 seconds');
 });
 
 noble.startScanning();
 
 
 
-
 return;
 
-peripherals.map(p => p.advertisement.localName);
-peripherals.map(p => p.advertisement.manufacturerData);
-peripherals = []; noble.startScanning();
+devices.map(d => d.advertisement.localName);
+devices.map(d => d.advertisement.manufacturerData);
+devices.map(d => d.address);
+devices.map(d => d.rssi);
+devices = []; noble.startScanning();
