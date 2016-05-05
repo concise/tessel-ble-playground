b871e283f105432731573ffa383834699910a568
Jong-Shian Wu <js@jong.sh>
2016-05-05 18:03:49 +0800

    -

--- tessel-as-beacon/index.js
+++ tessel-as-beacon/index.js
@@ -1,15 +1,22 @@
+console.log('INFO: Now we enter index.js');
+
 var tessel = require('tessel');
 var bleadvertise = require('bleadvertise');
 var ble_ble113a = require('ble-ble113a');
 
 var tessel_ble_module_ready = false;
+var tessel_ble_module_ready_pending_task = null;
+var default_advertisement_data = [0x01, 0x02, 0x03];
 
 var advertise_new_data = function (new_data) {
+  var _new_data_ = new Buffer(new_data);
+
   if (!tessel_ble_module_ready) {
-    console.log('Warning: Ignore advertise_new_data() function call since Tessel BLE module is not ready');
+    console.log('WARNING: Ignore advertise_new_data() function call since Tessel BLE module is not ready');
+    tessel_ble_module_ready_pending_task = function () { advertise_new_data(_new_data_); };
     return false;
   }
-  _new_data_ = new Buffer(new_data);
+
   var ad_data = bleadvertise.serialize({ flags: [0x04], mfrData: _new_data_ });
   tessel_ble_module.stopAdvertising(function () {
     console.log('Tessel BLE module stops advertising itself');
@@ -26,5 +33,12 @@ var advertise_new_data = function (new_data) {
 var tessel_ble_module = ble_ble113a.use(tessel.port['A'], function () {
   tessel_ble_module_ready = true;
   console.log('Tessel BLE module is ready');
-  advertise_new_data([0x01, 0x02, 0x03]);
+
+  if (tessel_ble_module_ready_pending_task === null) {
+    advertise_new_data(default_advertisement_data);
+  } else {
+    tessel_ble_module_ready_pending_task();
+  }
 });
+
+console.log('INFO: Now we reach the end of index.js');
