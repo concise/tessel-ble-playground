3aaf6a9d9c2bdddf9586af77c4d6b73c939ea577
Jong-Shian Wu <js@jong.sh>
2016-05-06 21:19:33 +0800

    -

--- thinkpad/index.js
+++ thinkpad/index.js
@@ -1,3 +1,5 @@
+var SENSOR_TIMEOUT = 3000;
+
 var noble = require('noble');
 
 noble.on('stateChange', function (state) {
@@ -17,7 +19,6 @@ noble.on('discover', function (device) {
     }
 });
 
-var TIMEOUT = 5000;
 var sensors = {};
 //
 //  'ndsensor-1': {
@@ -28,16 +29,16 @@ var sensors = {};
 //  }
 //
 
-var update_view = function () {
-    console.log('View:');
-    for (var name in sensors) {
-        var moni = sensors[name].moni;
-        var stat = sensors[name].stat;
-        var time = sensors[name].time;
-        var gone = sensors[name].gone;
-        console.log('  %s moni=%s stat=%d time=%d gone=%s', name, moni, stat, time, gone);
-    }
-};
+//var update_view = function () {
+//    console.log('View:');
+//    for (var name in sensors) {
+//        var moni = sensors[name].moni;
+//        var stat = sensors[name].stat;
+//        var time = sensors[name].time;
+//        var gone = sensors[name].gone;
+//        console.log('  %s moni=%s stat=%d time=%d gone=%s', name, moni, stat, time, gone);
+//    }
+//};
 
 var sensor_discovery_handler = function (name, stat) {
     var time = Date.now();
@@ -52,8 +53,8 @@ var sensor_discovery_handler = function (name, stat) {
         sensors[name].time = time;
         sensors[name].gone = false;
     }
-    setTimeout(timeout_checker, TIMEOUT, name, time);
-    update_view();
+    setTimeout(timeout_checker, SENSOR_TIMEOUT, name, time);
+    //update_view();
 };
 
 var timeout_checker = function (name, time) {
@@ -63,7 +64,7 @@ var timeout_checker = function (name, time) {
         } else {
             delete sensors[name];
         }
-        update_view();
+        //update_view();
     }
 };
 
@@ -71,12 +72,33 @@ var enable_monitoring = function (name) {
     if (sensors[name]) {
         sensors[name].moni = true;
     }
-    update_view();
+    //update_view();
 };
 
 var disable_monitoring = function (name) {
     if (sensors[name]) {
         sensors[name].moni = false;
     }
-    update_view();
+    //update_view();
 };
+
+
+
+require('./httpserver.js').run(function (set_port, set_hostname, set_handler) {
+
+    set_port(8000);
+    set_hostname('0.0.0.0');
+
+    set_handler('GET /', function (send_response) {
+        require('fs').readFile('index.html', function (err, data) {
+            if (err) throw err;
+            send_response(data, {'Content-Type': 'text/html; charset=utf-8'});
+        });
+    });
+
+    set_handler('POST /view', function (send_response, request_body, request_headers) {
+        var body = new Buffer(JSON.stringify(sensors), 'utf8');
+        send_response(body, {'Content-Type': 'application/octet-stream'});
+    });
+
+});
