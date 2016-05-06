ba2d2dc0957b9eb861583511f15e589fe91c2446
Jong-Shian Wu <js@jong.sh>
2016-05-06 17:01:33 +0800

    -

---
+++ thinkpad/index.js
@@ -0,0 +1,20 @@
+var noble = require('noble');
+
+noble.on('stateChange', function (state) {
+    if (state === 'poweredOn') {
+        noble.startScanning([], true);
+    }
+});
+
+noble.on('discover', function (device) {
+    var time = Date.now();
+    var rssi = device.rssi;
+    var name = device.advertisement.localName || '';
+    var data = device.advertisement.manufacturerData;
+    var stat = (data.length === 1 && data[0] === 0) ? 0 : 1;
+    if (name.startsWith('ndsensor-')) {
+        console.log('%d %s (-%d dbm) %d',
+            time, name, rssi, stat
+        );
+    }
+});
